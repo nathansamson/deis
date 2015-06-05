33e3333bcef46717b92dd442d6edc8ed0d22748d
Nathan Samson <nathan@nathansamson.be>
2015-06-05 22:35:05 +0200

    feat(client): Highlight changed config
    Highlight changed configuration after a config:set operation.

--- client/deis.py
+++ client/deis.py
@@ -1265,13 +1265,13 @@ Make sure that the Controller URI is correct and the server is running.
             version = response.headers['Deis-Release']
             self._logger.info("done, v{}\n".format(version))
             config = response.json()
-            values = config['values']
+            new_values = config['values']
             self._logger.info("=== {}".format(app))
-            items = values.items()
+            items = new_values.items()
             if len(items) == 0:
                 self._logger.info('No configuration')
                 return
-            self._display_configuration(values, oneline=False)
+            self._display_configuration(new_values, highlight=values.keys())
         else:
             raise ResponseError(response)
 
@@ -1335,13 +1335,20 @@ Make sure that the Controller URI is correct and the server is running.
 
         return env_dict
 
-    def _display_configuration(self, configuration, oneline=False):
+    def _display_configuration(self, configuration, **kwargs):
+        oneline = kwargs.get('oneline', False)
+        highlight = kwargs.get('highlight', [])
+
         keys = sorted(configuration)
         if not oneline:
             width = max(map(len, keys)) + 5
             for k in keys:
                 k, v = encode(k), encode(configuration[k])
-                self._logger.info(("{k:<" + str(width) + "} {v}").format(**locals()))
+                key = "{k:<" + str(width) + "}"
+                if k in highlight:
+                    self._logger.info(("\033[1m" + key + "\033[0m {v}").format(**locals()))
+                else:
+                    self._logger.info((key + " {v}").format(**locals()))
         else:
             output = []
             for k in keys:
